6bfaa2bbb0d4294a0b1f2874613bc55a09765829
Eduardo Ibanez <edu.ibanez@gmail.com>
2022-07-22 21:11:30 -0500

    Update process-issue.py

--- scripts/process-issue.py
+++ scripts/process-issue.py
@@ -1,3 +1,22 @@
 import os
 
-print(os.environ)
+new_entry = {'wordle_no': int(os.environ['ISSUE_TITLE'])}
+
+for d in os.environ['ISSUE_BODY'].upper().split():
+  if d[0] == 'J':
+    name = 'Jessica'
+  elif d[0] == 'E':
+    name = 'Eduardo'
+  elif d[0] == 'B':
+    name = 'Brian'
+  else:
+    raise RuntimeError('Invalid initial: {d[0]}')
+
+  if d[-1] == 'X':
+    score = 'x'
+  else:
+    score = int(d[-1])
+  
+  new_entry[name] = score
+
+print(new_entry)
